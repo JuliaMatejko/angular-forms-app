7c193a4a34823da79efe25074d96c8bcb0dd0b67
Julia Matejko <matejkojulia@gmail.com>
2025-03-17 02:57:15 +0100

    Adding a basic form control

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { NameEditorComponent } from "./name-editor/name-editor.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ReactiveFormsModule, NameEditorComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
